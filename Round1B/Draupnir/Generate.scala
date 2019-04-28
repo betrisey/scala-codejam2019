54efa8d368efcaa1f4e9b24f1b4b908fa34209b9
Samuel Bétrisey <samuel@sam.ninja>
2019-04-28 20:28:49 +0200

    Round 1B

--- Round1B/Draupnir/Generate.scala
+++ Round1B/Draupnir/Generate.scala
@@ -1,7 +1,20 @@
 package CodeJam2019.Round1B.Draupnir
 
+import scala.io.StdIn
+
 object Generate {
   def main(args: Array[String]): Unit = {
-
+    val mod = BigInt(2).pow(63)
+    print("Rings: ")
+    val rings = StdIn.readLine.split(' ').map(_.toInt)
+    println()
+    while (true) {
+      print("Day: ");
+      val day = StdIn.readLine().toInt
+      val res = (1 to 6).map(i => (BigInt(2).pow(day/i) * rings(i-1)) % mod)
+      println(res.mkString(" "))
+      println(res.sum % mod)
+      println()
+    }
   }
 }
