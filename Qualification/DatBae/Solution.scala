f69cda60dddcbff997175eb9a9ce2d7fda7c5e37
Samuel Bétrisey <samuel@sam.ninja>
2019-04-28 20:29:51 +0200

    Refactor qualifications

--- Qualification/DatBae/Solution.scala
+++ Qualification/DatBae/Solution.scala
@@ -5,7 +5,7 @@ import scala.io.StdIn
 
 object Solution {
   def main(args: Array[String]): Unit = {
-    for (i <- 1 to StdIn.readLine.toInt) {
+    for (_ <- 1 to StdIn.readLine.toInt) {
       //val n = Random.nextInt(1024) + 1
       //val brokenIndexTest = Seq.fill(Random.nextInt(16))(Random.nextInt(n)).toList.sorted.distinct
 
@@ -18,19 +18,15 @@ object Solution {
 
       val Array(n, b, f) = StdIn.readLine.split(" ").map(_.toInt)
 
-      val test1 = (0 until n).map(_ % 2 != 0).toArray
-      val test2 = (0 until n).map(_ / 2 % 2 != 0).toArray
-      val test4 = (0 until n).map(_ / 4 % 2 != 0).toArray
-      val test8 = (0 until n).map(_ / 8 % 2 != 0).toArray
-      val test16 = (0 until n).map(_ / 16 % 2 != 0).toArray
+      // Generate
+      // 1 -> 01010101...
+      // 2 -> 00110011...
+      // 3 -> 00001111...
+      // ...
+      val resMap = Iterator.iterate(1)(_ * 2).take(5)
+        .map(i => i -> testStore((0 until n).map(_ / i % 2 != 0).toArray))
+        .toMap
 
-      val res1 = testStore(test1)
-      val res2 = testStore(test2)
-      val res4 = testStore(test4)
-      val res8 = testStore(test8)
-      val res16 = testStore(test16)
-
-      val resMap = Map(16 -> res16, 8 -> res8, 4 -> res4, 2 -> res2, 1 -> res1)
       val brokenSet = mutable.TreeSet.empty[Int]
 
       def addInBroken(start: Int, length: Int, missing: Int, level: Int): Unit = {
@@ -51,7 +47,7 @@ object Solution {
       var drop = 0
       val last = Math.ceil(n / 16.0).toInt
       for (j <- 0 until last) {
-        val remaining = res16.drop(drop).takeWhile(x => x == (j % 2 != 0)).length
+        val remaining = resMap(16).drop(drop).takeWhile(x => x == (j % 2 != 0)).length
         val required = if (j == last && n % 16 != 0) n else 16
         addInBroken(drop, remaining, required - remaining, 16)
         drop += remaining
